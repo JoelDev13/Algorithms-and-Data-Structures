fab2a953ce159596a5e6dd34c05d6ac6563b3077
JoelDev13 <joel13r11@gmail.com>
2025-09-22 23:26:54 -0400

    feat: ejercio de recorrer array con csharp

---
+++ ESTRUCTURAS DE DATOS/Array/Recorrer Un Array/Ejercio1/Ejercio1/Program.cs
@@ -0,0 +1,11 @@
+﻿public class program{
+    public static void Main(String[] args){
+
+        int[] numero = {1,2,3,4,5};
+
+        for(int i = 0; i < numero.Length; i++)
+        {
+            Console.WriteLine(numero[i]);
+        }
+    }
+}
